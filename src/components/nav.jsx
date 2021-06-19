5d245db10aba1ea933cc3090c818608a61cd3d31
viiirus07 <viiirus07@gmail.com>
2021-06-19 21:54:50 +0530

    navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,8 +2,33 @@ import React from "react";
 import {Link} from "react-router-dom";
 import "../style/nav.css"
 function Nav(){
+
+
+        const navbar = React.useRef();
+        const [isClicked, setIsClicked] = React.useState(false);
+
+        
+        const navToggle = () => {
+            
+            if(isClicked){
+                navbar.current.style.display="none"
+               
+            }else{
+                navbar.current.style.display="flex";   
+            }
+        }
+
+    
+    
+
     return (<nav>
-                    <ul>
+  
+    <i onClick={()=>{
+        setIsClicked(!isClicked);
+        navToggle();
+    }} id="hamburger" className={isClicked===true?"fas fa-times":"fas fa-bars"} ></i>
+
+                    <ul ref={navbar} id="navbar">
                         <Link to="/">
                                       <li>Dasboard</li>
                         </Link>
